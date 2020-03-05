5f44b7cc902441855e6d3663df0c97e930484417
shailen <shailen@lumkani.com>
2020-03-05 16:16:47 +0200

    Minor change

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,15 @@
-import * as file from './File'
+import { normalize } from 'path'
 
-export { file }
+const CWD = process.cwd()
+
+async function importScript(filePath: string) {
+  const normalizedFilePath = normalize(`${CWD}/${filePath}`)
+
+  const module = await import(normalizedFilePath)
+
+  delete module.default
+
+  return module
+}
+
+export { importScript }
